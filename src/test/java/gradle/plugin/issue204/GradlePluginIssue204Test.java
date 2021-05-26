0c12a4b901d69bfe9e374b0ddb7817746721b603
Iván López <lopez.ivan@gmail.com>
2021-05-26 11:45:05 +0200

    Initial commit

---
+++ src/test/java/gradle/plugin/issue204/GradlePluginIssue204Test.java
@@ -0,0 +1,21 @@
+package gradle.plugin.issue204;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class GradlePluginIssue204Test {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
